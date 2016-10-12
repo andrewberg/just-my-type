53654f7acacea5558b7d39f1bc97f9ec222533d0
andrewberg <Andrew@wg-dhcp174d171d242.secure.wireless.fsu.edu>
2016-10-12 15:52:57 -0400

    Outline
    Added outline for typing test functionality

---
+++ JustMyType/TypingTest.swift
@@ -0,0 +1,24 @@
+//
+//  TypingTest.swift
+//  JustMyType
+//
+//  Created by Andrew on 10/12/16.
+//  Copyright © 2016 Lauren Koulias. All rights reserved.
+//
+
+import Foundation
+
+// TypeClass by Andrew and Andre
+
+// string for current word
+// getter and setter for the current word
+// array of random words
+// function to return a group of random words with a given size
+// function to check if the current word was typed correctly
+// function to advance to the next word
+// function to grab a random word from an array
+// function to shuffle around the source array and place into a new array
+
+class TypingTest {
+    
+}
