9b3b0feb73b49edd2984735ec256f4f1c5c1055c
Jorge Gonzalez <jrgqhd@gmail.com>
2016-10-18 13:43:23 -0400

    Add animation for new words

--- JustMyType/Views/TypingTestViewController.swift
+++ JustMyType/Views/TypingTestViewController.swift
@@ -20,6 +20,7 @@ class TypingTestViewController: UIViewController {
         {
             if (type.isCorrect(str: textFieldRef.text!))    //if correct word moves to next word
             {
+                wordLabel.shake()
                 wordLabel.text = type.getRandomWord()   //generate new random word to label
                 textFieldRef.text = ""                  //clear users text field
                 
@@ -46,4 +47,14 @@ class TypingTestViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+}
+
+extension UIView {
+  func shake() {
+    let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+    animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+    animation.duration = 0.6
+    animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+    layer.add(animation, forKey: "shake")
+  }
 }
