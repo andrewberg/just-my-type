b0529687c0055e3051a80c7151408c86f5e7ec5d
Jorge Gonzalez <jrgqhd@gmail.com>
2016-11-15 17:52:39 -0500

    Create BalloonViewController

---
+++ JustMyType/BalloonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BalloonViewController.swift
+//  JustMyType
+//
+//  Created by Jorge Gonzalez on 11/15/16.
+//  Copyright © 2016 Lauren Koulias. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class BalloonViewController: UIViewController {
+  
+  @IBOutlet weak var label1: UILabel!
+  @IBOutlet weak var label2: UILabel!
+  @IBOutlet weak var label3: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Release any cached data, images, etc that aren't in use.
+  }
+
+}
