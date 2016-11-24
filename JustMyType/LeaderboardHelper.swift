112a145ed8419b8afba16bfd96a77c58b51e9796
Andrew Berg <andrewberg@pobox.com>
2016-11-24 15:02:24 -0500

    Complete overhaul of Leaderboard. Added JSON to front end and backend. Added leaderboard entry for the TypingTest. Cleaned up Leaderboard class. TODO: Fix WPM Add the Leaderboard screen for TypingTest

--- JustMyType/LeaderboardHelper.swift
+++ JustMyType/LeaderboardHelper.swift
@@ -14,61 +14,85 @@ enum GameMode: String {
     case ballon = "ballonleader"
 }
 
-
 class Leaderboard {
-    var scores: [(name: String, score: Double)]
-    var mode: String
+    var scores:[(name: String, score: Double)]
+    var curMode: String
     var returnString: String
    
-    init() {
+    init(mode: String) {
         scores = []
         returnString = ""
-        mode = GameMode.typingtest.rawValue
+        curMode = Leaderboard.getMode(val: mode)
     }
     
-    // gets leaderboard html data for the given mode
-    func getLeaderboard() {
-        let url = URL(string: "http://104.131.38.84/jmt/\(mode)/list") // url for scoreboard
-        // run the request and store data in val
-        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
-            guard error == nil else {
-                print(error!)
-                return
-            }
-            guard let data = data else {
-                print("Data is empty")
-                return
-            }
-            
-            let html = String(data: data, encoding: String.Encoding.utf8)
-            self.fillArray(val: html!)
+    // sets the mode
+    private class func getMode(val: String) -> String {
+        switch val {
+        case "tt":
+            return GameMode.typingtest.rawValue
+        case "bl":
+            return GameMode.ballon.rawValue
+        case "bb":
+            return GameMode.basketball.rawValue
+        default:
+            return GameMode.typingtest.rawValue
         }
+    }
+
+    /* Example usage of getLeaderboard for x = Leaderboard()
+    x.getLeaderboard()
+        {(scores: [(name: String, score: Double)]) -> (Void) in
+            for x in scores {
+            print("\(x.0) \(x.1)")
+        }
+    }
+    */
+
+    // gets leaderboard html data for the given mode
+    func getLeaderboard(completionHandler:@escaping ([(name: String, score: Double)]) -> ()) {
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: "http://104.131.38.84/jmt/\(curMode)/list")
+        
+        let task = session.dataTask(with: url!, completionHandler: { // creates task to pickup json
+            (data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+                    // grabs json data from data
+                    {
+                        if let pairs = json["score_list"] as? [[String: AnyObject]] { // accesses score_list dict
+                            for pair in pairs { // loops through pairs and then adds to scores
+                                let name = pair["name"]!
+                                let score = pair["score"]!
+                                
+                                self.scores.append((name: name as! String, score: score as! Double))
+                            }
+                        }
+                        completionHandler(self.scores) // calls completionHandler when done adding values
+                        return
+                    }
+                } catch {
+                    print("error in getList()")
+                }
+            }
+        })
         task.resume()
     }
     
-    // parses html data
-    func fillArray(val: String) {
-        var split0 = val.components(separatedBy: "<body>")
-        var split1 = split0[1].components(separatedBy: "<br/>")
-        split1.remove(at: split1.endIndex-1)
-        // splitting magic
-        for x in split1 {
-            let splitted = x.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: " ")
-            self.scores.append((name:splitted[0], score: Double(splitted[1])!))
-        }
-    }
-    
+    // enters score by post method to given database
     func enterScore(name: String, score: Double) {
-        
-        var request = URLRequest(url: URL(string: "http://104.131.38.84/jmt/\(mode)/enter")!)
-        request.httpMethod = "POST"
-        let sent = "name=\(name)&score=\(score)"
+        var request = URLRequest(url: URL(string: "http://104.131.38.84/jmt/\(curMode)/enter")!)
+        request.httpMethod = "POST" // sets method to post
+        let sent = "name=\(name)&score=\(score)" // sets the parameters
         request.httpBody = sent.data(using: .utf8)
-        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in // establishes request
             guard let _ = data, error == nil else {
                 return
             }
-            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 { // handles the invalid response
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
                 print("response = \(response)")
             }
