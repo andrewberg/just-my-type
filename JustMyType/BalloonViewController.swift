67bfe20f2a784f16486c950dd372f7d2fba75cfc
Jorge Gonzalez <jrgqhd@gmail.com>
2016-11-29 13:27:56 -0500

    Add initial functionality to the Balloon game

--- JustMyType/BalloonViewController.swift
+++ JustMyType/BalloonViewController.swift
@@ -11,50 +11,50 @@ import SpriteKit
 
 class BalloonViewController: UIViewController {
     
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var label3: UILabel!
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var words: UILabel!
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var score: UILabel!
     
     var test = TypingTest()
-    var score = 0
+    var gameScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initText()
         initStyle()
+        words.text = test.getsWordsToDisplay()
+        input.addTarget(self, action: #selector(self.textField(_:)), for: UIControlEvents.editingChanged)
     }
     
-    @IBAction func click(_ sender: Any) {
-        main()
+    @IBAction func textField(_ sender: Any) {
+        if input.text != "" {
+            let entry = input.text?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            
+            if test.isCorrect(str: entry!) {
+                gameScore += 5
+                score.text = String(gameScore)
+                test.makeCurWordNextWord()
+                words.text = test.getsWordsToDisplay()
+                input.text = ""
+            }
+        }
     }
-    
-    func initText() {
-        // Load 3 words from TypingTest
-        label1.text = ""
-        label2.text = ""
-        label3.text = ""
-        textField.text = "Pop the balloons!"
-        
-    }
-    
     
     func initStyle() {
         // draws 3 balloons at x,y positions
         addBalloon(move: 80, down: 160)
         addBalloon(move: 180, down: 110)
         addBalloon(move: 290, down: 160)
-        
+        score.text = String(gameScore)
     }
     
     // adds a single balloon at x,y positions - Asa
-    func addBalloon(move: Int, down: Int){
+    func addBalloon(move: Int, down: Int) {
         let color = randomColor().cgColor // set balloon colors
         drawBalloon(move: move, down: down, color: color)
     }
     
     // draws the balloon - Asa
-    func drawBalloon(move: Int, down: Int, color: CGColor){
+    func drawBalloon(move: Int, down: Int, color: CGColor) {
         let circlePath = UIBezierPath(arcCenter: CGPoint(x: move,y: down), radius: CGFloat(35), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
         
         let label = CAShapeLayer()
@@ -63,14 +63,13 @@ class BalloonViewController: UIViewController {
         label.fillColor =  color
         label.strokeColor = color
         label.lineWidth = 0.5
-        addText(circleLayer: label, move: move, down: down) // add text layer to label
+        // addText(circleLayer: label, move: move, down: down) // add text layer to label
         addHandle(circleLayer: label ,move: move, down: down, color: color) // add handle to label
         view.layer.addSublayer(label)
-        
     }
     
     // add text to layer - Asa
-    func addText(circleLayer: CAShapeLayer, move: Int, down: Int){
+    func addText(circleLayer: CAShapeLayer, move: Int, down: Int) {
         let txtLyr = CATextLayer()
         txtLyr.bounds = CGRect(x:0,y: 0,width: 50,height: 25)
         txtLyr.position = CGPoint(x:move,y: down)
@@ -106,41 +105,18 @@ class BalloonViewController: UIViewController {
         handle.strokeColor = color
         handle.lineWidth = 0.5
         circleLayer.addSublayer(handle)
-        
     }
     
-    
     // returns a random color - Asa
-    func randomColor() -> UIColor{
+    func randomColor() -> UIColor {
         let red = CGFloat(drand48())
         let green = CGFloat(drand48())
         let blue = CGFloat(drand48())
         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
     
-    func moveUILabel(label: UILabel) {
-        label.frame.origin.y -= 1;
-    }
-    
-    
-    func updateUI() {
-        moveUILabel(label: label1)
-        moveUILabel(label: label2)
-        moveUILabel(label: label3)
-    }
-    
-    func getNewWords() {
-        label1.text = test.addRandomWordAndGetValue()
-    }
-    
-    func main() {
-        textField.text = ""
-        initStyle()
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-    
 }
